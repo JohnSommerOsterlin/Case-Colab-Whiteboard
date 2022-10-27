8c465811b905b9e74a312a7e109f94c787ded4e1
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2022-10-27 12:36:31 +0200

    Code clean up and removed unnecessary console.logs

--- server/server.mjs
+++ server/server.mjs
@@ -1,6 +1,5 @@
 import express from "express";
 import { WebSocketServer } from "ws";
-// import http from "http";
 
 // creating websocket server
 const wss = new WebSocketServer({ noServer: true });
@@ -11,15 +10,18 @@ const expressServer = app.listen(port, () =>
   console.log(`Running on port ${port}`)
 );
 
+// Handeling upgrade
 expressServer.on("upgrade", (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (websocket) => {
     wss.emit("connection", websocket, request);
   });
 });
 
+// Displaying the number of clients
 wss.on("connection", (ws, request, client) => {
   console.log("Number of connected clients: ", wss.clients.size);
 
+  // Handeling the data from the client
   ws.on("message", function message(data) {
     const obj = JSON.parse(data);
     console.log("recieved: %s", obj.type);
