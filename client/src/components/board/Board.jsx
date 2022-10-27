6558adfaaf07db3c08961c3710c4ecd302ef9826
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2022-10-27 12:28:33 +0200

    Moved README from client to the root file

--- client/src/components/board/Board.jsx
+++ client/src/components/board/Board.jsx
@@ -1,131 +1,128 @@
-import React from 'react'
-import { useEffect, useRef, useState } from 'react';
-import "./board.css"
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+import "./board.css";
 
-const ws = new WebSocket("ws://localhost:8080")
+const ws = new WebSocket("ws://localhost:8080");
 
 export default function Board() {
+  let paintObj = {
+    color: "black",
+    type: "paint",
+    coordinates: [],
+  };
 
-    let paintObj = {
-        color: "black",
-        type: "paint",
-        coordinates: [],
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  // const [color, setColor] = useState("")
+
+  ws.onclose = (evt) => console.log("Closing", evt);
+  ws.onopen = (evt) => console.log("Open", evt);
+  ws.onmessage = (evt) => {
+    const obj = JSON.parse(evt.data);
+    switch (obj.type) {
+      case "paint":
+        paintLine(contextRef, obj.coordinates);
+        break;
+      default:
+        console.log("default case");
     }
-    
-    
-    
-    const canvasRef = useRef(null)
-    const contextRef = useRef(null)
-    const [isDrawing, setIsDrawing] = useState(false)
-    // const [color, setColor] = useState("")
-    
-    
-    ws.onclose = (evt) => console.log("Closing", evt);
-    ws.onopen = (evt) => console.log("Open", evt);
-    ws.onmessage = (evt) => {
-        const obj = JSON.parse(evt.data);
-        switch (obj.type) {
-            case "paint":
-                paintLine(contextRef, obj.coordinates)                
-                break;
-                default:
-                    console.log("default case")
-                    
-                };
-            };
-            
-            function paintLine(context, coordinates) {
-                context.current.beginPath()
-                coordinates.forEach(e => {
-                    console.log(e)
-                    context.current.moveTo(e.x, e.y)
-                    context.current.lineTo(e.x, e.y)
-                    context.current.stroke()
-                    context.current.beginPath()
-                });
-            }
-    
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        canvas.width = 1080;
-        canvas.height = 720;
-        
-        const context = canvas.getContext("2d")
-        context.lineCap = "round";
-        context.strokeStyle = "black"
-        context.lineWidth = 10;
-        contextRef.current = context
-        
-        
-    }, []);
+  };
 
-    
-    // const colorPicker = () => {
-    //     const [color, setColor] = useState(null)
-    //     console.log("colorPicker", color);
-    // }
+  function paintLine(context, coordinates) {
+    context.current.beginPath();
+    coordinates.forEach((e) => {
+      console.log(e);
+      context.current.moveTo(e.x, e.y);
+      context.current.lineTo(e.x, e.y);
+      context.current.stroke();
+      context.current.beginPath();
+    });
+  }
 
-    const startDrawing = ({nativeEvent}) => {
-        const {offsetX, offsetY} = nativeEvent;
-        contextRef.current.beginPath();
-        contextRef.current.moveTo(offsetX, offsetY);
-        contextRef.current.lineTo(offsetX, offsetY);
-        contextRef.current.stroke();
-        setIsDrawing(true);
-        nativeEvent.preventDefault();
-    };
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = 1080;
+    canvas.height = 720;
+    const context = canvas.getContext("2d");
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 10;
+    contextRef.current = context;
+  }, []);
 
+  // const colorPicker = () => {
+  //     const [color, setColor] = useState(null)
+  //     console.log("colorPicker", color);
+  // }
 
-    const draw = ({nativeEvent}) => {
-        if(!isDrawing) {
-            return;
-        }
-        const {offsetX, offsetY} = nativeEvent;
-        contextRef.current.lineTo(offsetX, offsetY);
-        contextRef.current.stroke();
-        nativeEvent.preventDefault();
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+    setIsDrawing(true);
+    nativeEvent.preventDefault();
+  };
 
-        let coordinates = {x: nativeEvent.offsetX, y: nativeEvent.offsetY}
-        paintObj.coordinates.push(coordinates)
-    };
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+    nativeEvent.preventDefault();
 
-    const stopDrawing = () => {
-        contextRef.current.closePath();
-        setIsDrawing(false);
-        ws.send(JSON.stringify(paintObj))
-        
-    };
+    let coordinates = { x: nativeEvent.offsetX, y: nativeEvent.offsetY };
+    paintObj.coordinates.push(coordinates);
+  };
 
-    const setToDraw = () => {
-        contextRef.current.globalCompositeOperation = "source-over"
-    };
+  const stopDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+    ws.send(JSON.stringify(paintObj));
+  };
 
-    const setToErase = () => {
-        contextRef.current.globalCompositeOperation = "destination-out"
+  const setToDraw = () => {
+    contextRef.current.globalCompositeOperation = "source-over";
+  };
 
-    };
+  // const setToErase = () => {
+  //     contextRef.current.globalCompositeOperation = "destination-out"
 
+  // };
 
-    return (
-        <div>
-            <canvas className="board" 
-                ref={canvasRef}
-                onMouseDown={startDrawing}
-                onMouseMove={draw}
-                onMouseUp={stopDrawing}
-                onMouseLeave={stopDrawing}
-            >
-            </canvas>
-            <div className='btn-color-container'>
-                <input className='color-picker' type="color" id="color" />
-                <button className='draw-btn' onClick={setToDraw}>
-                    Draw
-                </button>
-                <button className='erase-btn' onClick={setToErase}>
-                    Erase
-                </button>
-            </div>
-        </div>
-            
-    )
+  const reset = () => {
+    contextRef.current.clearRect(
+      10,
+      10,
+      contextRef.current.width,
+      contextRef.current.height
+    );
+    console.log("clicked");
+  };
+
+  return (
+    <div>
+      <canvas
+        className="board"
+        ref={canvasRef}
+        onMouseDown={startDrawing}
+        onMouseMove={draw}
+        onMouseUp={stopDrawing}
+        onMouseLeave={stopDrawing}
+      ></canvas>
+      <div className="btn-color-container">
+        <input className="color-picker" type="color" id="color" />
+        <button className="draw-btn" onClick={setToDraw}>
+          Draw
+        </button>
+        <button className="erase-btn" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
--- server/server.mjs
+++ server/server.mjs
@@ -2,50 +2,42 @@ import express from "express";
 import { WebSocketServer } from "ws";
 // import http from "http";
 
-
 // creating websocket server
-const wss = new WebSocketServer({noServer: true});
+const wss = new WebSocketServer({ noServer: true });
 const app = express();
 app.use(express.static("../client/whiteboard/build"));
 const port = 8080;
-const expressServer = app.listen(port, () => console.log(`Listening on port ${port}`));
+const expressServer = app.listen(port, () =>
+  console.log(`Running on port ${port}`)
+);
 
 expressServer.on("upgrade", (request, socket, head) => {
-    console.log("Upgrading...");
-    wss.handleUpgrade(request, socket, head, (websocket) => {
-        console.log("handeling upgrade");
-        wss.emit("connection", websocket, request)
-    });
+  wss.handleUpgrade(request, socket, head, (websocket) => {
+    wss.emit("connection", websocket, request);
+  });
 });
 
-
 wss.on("connection", (ws, request, client) => {
-    // console.log("New client connection from IP: %s", ws._socket.remoteAdress);
-    console.log("Number of connected clients: ", wss.clients.size);
+  console.log("Number of connected clients: ", wss.clients.size);
 
-    
-    ws.on("message", function message(data) {
-        const obj = JSON.parse(data);
-        console.log("recieved: %s", obj.type);
-        switch (obj.type) {
-            case "paint":
-                {
-                    console.log("Broadcasting:", obj);
-                    wss.clients.forEach((client) => client.send(JSON.stringify(obj)));
-                }
-                break;
-                default:
-                    console.log("Default")
+  ws.on("message", function message(data) {
+    const obj = JSON.parse(data);
+    console.log("recieved: %s", obj.type);
+    switch (obj.type) {
+      case "paint":
+        {
+          console.log("Broadcasting:", obj);
+          wss.clients.forEach((client) => client.send(JSON.stringify(obj)));
         }
-        // ws.send("Thank you for your message!")
-    });
+        break;
+      default:
+        console.log("Default");
+    }
+  });
 
-    // close event
-    ws.on("close", () => {
-        console.log("Client disconnected");
-        console.log(
-            "Number of remaining connected clients: ",
-            wss.clients.size
-            );
-    });
+  // close event
+  ws.on("close", () => {
+    console.log("Client disconnected");
+    console.log("Number of remaining connected clients: ", wss.clients.size);
+  });
 });
